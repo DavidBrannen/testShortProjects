07846dc18c762331620e67bf30aa4e14fb93d0ce
David Brannen <david.brannen@Mobileappscompany@us>
2019-03-19 20:52:05 -0400

    Update FunProjectTests.swift

--- FunProjectTests/FunProjectTests.swift
+++ FunProjectTests/FunProjectTests.swift
@@ -8,23 +8,25 @@
 import XCTest
 
 class FunProjectTests: XCTestCase {
-
-
+    
+    //1. Find the number of occurrences of an integer in a sorted array. (logarithmic solution preferred)
     func testProblem1() {
         let inputs = [1, 2, 3, 3, 3, 3, 3, 4, 5, 5, 6, 7]
-        let target = 3
+        let k = 3
         let solutions = 5
-        let result = Problem1.perform(inputs, target)
+        let result = Problem1.perform(inputs, k)
         let solution = solutions
-        XCTAssertEqual(result, solution, "\(inputs), \(target) caused an error")
+        XCTAssertEqual(result, solution, "\(inputs), \(k) caused an error")
     }
+    //2. Find the second-largest integer of a non-sorted array (also including cases for negative values).
     func testProblem2() {
-        let inputs = [-1, -12, -17, -5, -14, -5]
-        let solutions = -5
+        let inputs = [-1, -12, -17, -5, -14, -5, 5, 5, 4]
+        let solutions = 4
         let result = Problem2.perform(inputs)
         let solution = solutions
         XCTAssertEqual(result, solution, "\(inputs) caused an error")
     }
+    //3. Given an array and a sum, return indices of 2 integers within the array that total to that sum. Solve this in Linear runtime complexity
     func testProblem3() {
         let inputs = [1, 2, 3, 4, 5]
         let sum = 7
@@ -36,6 +38,7 @@ class FunProjectTests: XCTestCase {
         XCTAssertEqual(result1, solution1, "\(inputs) caused an error1")
         XCTAssertEqual(result2, solution2, "\(inputs) caused an error2")
     }
+    //4. Given an array and a value k, return the number of contiguous subarrays whose total product <= k. Assume all elements in array are integers > 0 and k is > 0. Provide a linear runtime complexity.
     func testProblem4() {
         let inputs = [3, 2, 60, 4, 5, 2]
         let k = 40
@@ -44,18 +47,77 @@ class FunProjectTests: XCTestCase {
         let solution = solutions
         XCTAssertEqual(result, solution, "\(inputs) caused an error")
     }
-
+    //fahad C H /sorted array - find the occurrence of the given key
+    func testProblem5() {
+        let inputs = [1,2,3,3,3,3,3,3,3,4,5,5,5,5,6,6,6,6,6,6,6,7,8,33,45,66,78]
+        //        same as 1
+        let k = 3
+        let solutions = 7
+        let result = Problem5.occurrences(inputs, k)
+        let solution = solutions
+        XCTAssertEqual(result, solution, "\(inputs) caused an error")
+    }
+    //binary search - sorted- find the occurrence of the given key
+    func testProblem6() {
+        let inputs = [1,2,3,3,3,3,3,3,3,4,5,5,5,5,6,6,6,6,6,6,6,7,8,33,45,66,78]
+        let k = 5
+        let result = Problem6.binaryOccurrences(inputs, k)
+        let solution = 4
+        XCTAssertEqual(result, solution, "\(inputs) count \(k) caused an error")
+    }
+}
+// binary search for occurrenence count
+struct Problem6 {
+    static func binaryOccurrences(_ arr: [Int], _ k: Int) -> Int {
+        func leftBound () -> Int {
+            var lowerBound = 0
+            var upperBound = arr.count
+            while lowerBound < upperBound {
+                let midIndex = lowerBound + (upperBound - lowerBound) / 2
+                if arr[midIndex] < k {
+                    lowerBound = midIndex + 1
+                } else  {
+                    upperBound = midIndex
+                }
+            }
+            return lowerBound
+        }
+        
+        func rightBound () -> Int {
+            var lowerBound = 0
+            var upperBound = arr.count
+            while lowerBound < upperBound {
+                let midIndex = lowerBound + (upperBound - lowerBound) / 2
+                if arr[midIndex] > k {
+                    upperBound = midIndex
+                } else  {
+                    lowerBound = midIndex + 1
+                }
+            }
+            return lowerBound
+        }
+        
+        return rightBound() - leftBound()
+    }
 }
 
+//***********
+//fahad C H /sorted array - find the occurrence of the given key
+struct Problem5 {
+    static func occurrences(_ arr: [Int],_ k: Int) -> Int {
+        return arr.reduce(0) { return $0 + ($1 == k ? 1 : 0) }
+        //Do with binary search above
+    }
+}
 //1. Find the number of occurrences of an integer in a sorted array. (logarithmic solution preferred)
 struct Problem1 {
-    static func perform(_ arr: [Int],_ target: Int) -> Int {
+    static func perform(_ arr: [Int],_ k: Int) -> Int {
         var intCount = 0
         for r in arr {
-            if r == target {
+            if r == k {
                 intCount += 1
             }
-            if r > target {break}
+            if r > k {break}
         }
         return intCount
     }
@@ -63,26 +125,28 @@ struct Problem1 {
 
 //2. Find the second-largest integer of a non-sorted array (also including cases for negative values).
 struct Problem2 {
-    static func perform(_ arr: [Int]) -> Int {
+    static func perform(_ arr: [Int]) -> Int? {
         var largest = arr[0]
-        var second = arr[1]
-        if second > largest {
-            largest = second
-            second = arr[0]
-        }
+        var second: Int?
+        
         for i in 1..<arr.count {
             if arr[i] >= largest {
-                if largest > second {
-                    second = largest
-                }
                 largest = arr[i]
-            } else {
-                if arr[i] > second {
-                    second = arr[i]
-                }
             }
         }
-        return second
+        for i in 0..<arr.count {
+            if arr[i] < largest {
+                second = arr[i]
+                break
+            }
+        }
+        if second == nil { return nil }
+        for i in 0..<arr.count {
+            if arr[i] > second! && arr[i] != largest {
+                second = arr[i]
+            }
+        }
+        return second!
     }
 }
 
@@ -122,6 +186,54 @@ struct Problem4 {
             }
         }
         return count
-
+        
     }
 }
+////////////
+//888888888888*******8888
+// equate enum case
+enum person {
+    
+    case student(name: String)
+    
+    case teacher(name: String)
+}
+// equate enum case... which has... associated values. Yeah, the static comparison method
+//Let's implement the Equatable protocol.
+extension person: Equatable {
+    static func == (lhs: person, rhs: person) -> Bool {
+        var lhsVal: String? = nil
+        switch(lhs) {
+        case .student(let name):
+            lhsVal = name
+        case .teacher(let name):
+            lhsVal = name
+        }
+        
+        var rhsVal: String? = nil
+        switch(rhs) {
+        case .student(let name):
+            rhsVal = name
+        case .teacher(let name):
+            rhsVal = name
+        }
+        return lhsVal == rhsVal
+    }
+}
+let student1 = person.student(name: "Foo")
+let teacher1 = person.teacher(name: "Foo")
+
+let op = Operation.init()
+
+let blockOp = BlockOperation {
+    if student1 == teacher1 {
+        print("They are the same")
+    }
+    else {
+        print("They are not the same")
+    }
+}
+
+
+
+
